0e8e1cce7ac000ec437cb4f8e8811b9be8411203
swarupnihinmedia <swarup.baidya@nihinmedia.jp>
2025-05-15 02:27:28 +0530

    starting

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const {ApolloServer} = require("@apollo/server");
+const {expressMiddleware} = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs : `
+        type Todo {
+        id : ID!
+        title : String!
+        completed: boolean
+        }
+        `,
+        resolvers: {}
+    });
+
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    await server.start();
+
+    app.use("/graphql",expressMiddleware(server));
+
+    app.listen(8000 , ()=>console.log("server start at port 8000"));
+}
+
+startServer();
