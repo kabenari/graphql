03b8703fd6a5f620b3c861d424e4c326b348809e
swarupnihinmedia <swarup.baidya@nihinmedia.jp>
2025-05-15 02:29:11 +0530

    starting

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const {ApolloServer} = require("@apollo/server");
-const {expressMiddleware} = require("@apollo/server/express4");
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-
 async function startServer() {
-    const app = express();
-    const server = new ApolloServer({
-        typeDefs : `
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
         type Todo {
         id : ID!
         title : String!
         completed: boolean
         }
+
+        type Query{
+        
         `,
-        resolvers: {}
-    });
+    resolvers: {},
+  });
 
-    app.use(bodyParser.json());
-    app.use(cors());
+  app.use(bodyParser.json());
+  app.use(cors());
 
-    await server.start();
+  await server.start();
 
-    app.use("/graphql",expressMiddleware(server));
+  app.use("/graphql", expressMiddleware(server));
 
-    app.listen(8000 , ()=>console.log("server start at port 8000"));
+  app.listen(8000, () => console.log("server start at port 8000"));
 }
 
 startServer();
